749b02bc7627cb84de49cb86326843893921bb52
Jon Welker <JWelker110@gmail.com>
2015-02-17 17:25:58 -0600

    Update movie_hub to accomodate extra movie data

--- movie_hub.py
+++ movie_hub.py
@@ -1,11 +1,19 @@
 import fresh_tomatoes
-from media import media
+from media import movie
 import json
 
 
-movies = json.load(open("movie_list.json"))
+movies = json.load(open("movie_list.json"))["movies"]
 
-fresh_tomatoes.open_movies_page(movies)
+movie_list = []
 
+for m in movies:
+    movie_list.append(movie.Movie(m["title"],
+                                  m["description"],
+                                  m["poster_image"],
+                                  m["youtube_trailer"],
+                                  m["rating"],
+                                  m["duration"],
+                                  m["genre"]))
 
-
+fresh_tomatoes.open_movies_page(movie_list)
