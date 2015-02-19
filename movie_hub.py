08cef8657704cdebcdff91ea5d2ac9c98a0fe84b
Jon Welker <JWelker110@gmail.com>
2015-02-19 15:52:32 -0600

    Add comments for code clarity

--- movie_hub.py
+++ movie_hub.py
@@ -2,11 +2,12 @@ import fresh_tomatoes
 from media import movie
 import json
 
-
+# reading movie details from json
 movies = json.load(open("movie_list.json"))["movies"]
 
 movie_list = []
 
+# iterating through each json object and adding it to the movie list
 for m in movies:
     movie_list.append(movie.Movie(m["title"],
                                   m["description"],
@@ -16,4 +17,5 @@ for m in movies:
                                   m["duration"],
                                   m["genre"]))
 
+# generating the html for movie hub using the movie list
 fresh_tomatoes.open_movies_page(movie_list)
